1f8b21d1ce7d366f6bd660c010ba96cbbf1fb7fc
avendesta <53615807+avendesta@users.noreply.github.com>
2021-08-10 10:07:26 -0500

    Part 2: added models

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the polls index")
+    return render(request, 'polls/index.html')
